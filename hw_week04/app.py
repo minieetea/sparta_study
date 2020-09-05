e79ed0144a0b1218769bfd8fc9997b693ee9c2a6
minieetea <kang@mikyung.me>
2020-09-06 04:59:25 +0900

    Api app 코드 작성

--- hw_week04/app.py
+++ hw_week04/app.py
@@ -17,16 +17,32 @@ def homework():
 # 주문하기(POST) API
 @app.route('/order', methods=['POST'])
 def save_order():
-# 여길 채워나가세요!
+    # 주문자이름, 수량, 주소, 전화번호을 받아온다.
+    name_receive = request.form['name_give']
+    quantity_receive = request.form['quantity_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
+    print("받아온 값:", name_receive, quantity_receive, address_receive, phone_receive)
 
+    # 2. mongoDB에 데이터 넣기
+    order = {
+        'name': name_receive,
+        'quantity': quantity_receive,
+        'address': address_receive,
+        'phone': phone_receive
+    }
+    db.myshops.insert_one(order)
     return jsonify({'result': 'success'})
-
+  
 
 # 주문 목록보기(Read) API
 @app.route('/order', methods=['GET'])
 def view_orders():
-# 여길 채워나가세요!
-    return jsonify({'result': 'success', 'orders': orders })
+    # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기(Read)
+    orders = list(db.alonememo.find({}, {'_id': False}))
+
+    # 2. orders라는 키 값으로 주문정보 보내주기
+    return jsonify({'result': 'success', 'orders': orders})
 
 
 if __name__ == '__main__':
