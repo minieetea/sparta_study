f9d06d3477af2b0d165107201b7783e1b955a00c
minieetea <kang@mikyung.me>
2020-09-06 05:18:19 +0900

    클라이언트 코드 작성
    주문 API 연결
    주문조회 API 연결
    주문조회 API 응답결과 출력

--- hw_week04/app.py
+++ hw_week04/app.py
@@ -19,27 +19,27 @@ def homework():
 def save_order():
     # 주문자이름, 수량, 주소, 전화번호을 받아온다.
     name_receive = request.form['name_give']
-    quantity_receive = request.form['quantity_give']
+    count_receive = request.form['count_give']
     address_receive = request.form['address_give']
     phone_receive = request.form['phone_give']
-    print("받아온 값:", name_receive, quantity_receive, address_receive, phone_receive)
+    print("받아온 값:", name_receive, count_receive, address_receive, phone_receive)
 
     # 2. mongoDB에 데이터 넣기
     order = {
         'name': name_receive,
-        'quantity': quantity_receive,
+        'count': count_receive,
         'address': address_receive,
         'phone': phone_receive
     }
     db.myshops.insert_one(order)
-    return jsonify({'result': 'success'})
-  
+    return jsonify({'result': 'success', 'msg': '성공ㅆ~'})
+
 
 # 주문 목록보기(Read) API
-@app.route('/order', methods=['GET'])
+@app.route('/orders', methods=['GET'])
 def view_orders():
     # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기(Read)
-    orders = list(db.alonememo.find({}, {'_id': False}))
+    orders = list(db.myshops.find({}, {'_id': False}))
 
     # 2. orders라는 키 값으로 주문정보 보내주기
     return jsonify({'result': 'success', 'orders': orders})
